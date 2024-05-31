32f43901128bd437e5ed8b8e2411329c4c0e3430
mecrophagus <122439515+mecrophagus@users.noreply.github.com>
2024-05-31 05:55:06 +0000

    agragando lo principal

--- src/app.js
+++ src/app.js
@@ -8,4 +8,27 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "my", "your"];
+  let adj = ["great", "big", "awesome", "small"];
+  let noun = ["jogger", "racoon", "developer", "car"];
+  let extensions = [".com", ".es", ".org", ".domain", ".io", ".blockchain"];
+
+  // Obtener la referencia del elemento UL en el HTML
+  let domainList = document.getElementById("domain-list");
+
+  // Iterar sobre cada elemento de las listas para generar las combinaciones
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let ext of extensions) {
+          // Concatenar las palabras y agregar la extensión del dominio
+          let domain = p + a + n + ext;
+          // Crear un nuevo elemento LI y agregar el dominio generado
+          let listItem = document.createElement("li");
+          listItem.textContent = domain;
+          domainList.appendChild(listItem);
+        }
+      }
+    }
+  }
 };
