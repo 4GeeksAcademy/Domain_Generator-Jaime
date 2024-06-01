23232aa75a81f95856c21a162c770c0475f84780
mecrophagus <122439515+mecrophagus@users.noreply.github.com>
2024-06-01 10:25:32 +0000

    Fastidioso prettier

--- src/app.js
+++ src/app.js
@@ -1,29 +1,28 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  // write your code here
   console.log("Hello Rigo from the console!");
   let pronoun = ["the", "our", "my", "your"];
   let adj = ["great", "big", "awesome", "small"];
   let noun = ["jogger", "racoon", "developer", "car"];
   let extensions = [".com", ".es", ".org", ".domain", ".io", ".blockchain"];
 
-  // Obtener la referencia del elemento UL en el HTML
+  // Obtener la referencia del elemento UL en el HTML - obtain the reference of the element UL in the HTML
   let domainList = document.getElementById("domain-list");
 
-  // Iterar sobre cada elemento de las listas para generar las combinaciones
+  // Iterar sobre cada elemento de las listas para generar las combinaciones - Iterate over every element of the list to generate the combinations.
   for (let p of pronoun) {
     for (let a of adj) {
       for (let n of noun) {
         for (let ext of extensions) {
-          // Concatenar las palabras y agregar la extensión del dominio
+          // Concatenar las palabras y agregar la extensión del dominio - Concatenate the words and add the extension of the domain.
           let domain = p + a + n + ext;
-          // Crear un nuevo elemento LI y agregar el dominio generado
+          // Crear un nuevo elemento LI y agregar el dominio generado -
           let listItem = document.createElement("li");
           listItem.textContent = domain;
           domainList.appendChild(listItem);
